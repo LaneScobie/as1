41b880d366e03fdcb5c5a22ddf1944d14f515cf9
LaneScobie <scobie@ualberta.ca>
2016-09-29 19:21:38 -0600

    Here we go!

---
+++ app/src/main/java/com/example/scobie/habittracker/Habit.java
@@ -0,0 +1,18 @@
+package com.example.scobie.habittracker;
+
+import java.util.Date;
+
+/**
+ * Created by scobie on 9/29/16.
+ */
+public class Habit  {
+    public String name;
+    public Date date;
+
+    public Habit(String name){
+        this.name= name;
+    }
+
+
+
+}
